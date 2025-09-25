f3bb5173d62ad0ffbc99bb11c5371f4da22687f9
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-26 00:17:08 +0530

    push

--- myproject2/app/admin.py
+++ myproject2/app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
 # Register your models here.
+
+# d = {
+#     'name':"Amit chauhan",
+#     'place':"India",
+#     'age': 30,
+    
+# }
+
+# for key in d:
+#     print(key)
--- myproject2/app/views.py
+++ myproject2/app/views.py
@@ -31,3 +31,17 @@ def table(request):
     }
     return render(request, "table.html", data)
 
+
+def tab(request):
+    data = {
+        'student' :[
+            {'name':"Amit chauhan", 'roll':101, 'marks':85},
+            {'name':"Rahul sharma", 'roll':102, 'marks':90},
+            {'name':"Ankit singh", 'roll':103, 'marks':78},
+            {'name':"Vikas yadav", 'roll':104, 'marks':88},
+            {'name':"Rohit kumar", 'roll':105, 'marks':92},
+        ]
+        
+    }
+    return render(request, "tab.html", {"data": data})
+
