f2cec258b6915730b0cabaa3e2b596e28a83c8e0
amitcoderhub <amitchauhan6599@gmail.com>
2025-09-25 23:10:55 +0530

    push

--- myproject2/app/views.py
+++ myproject2/app/views.py
@@ -22,6 +22,7 @@ def data(request):
 def table(request):
     data = {
         'numbers': range(1, 7),
+       
         'squares': [i**2 for i in range(1, 7)],
         'cubes': [i**3 for i in range(1, 7)],
         'fourths': [i**4 for i in range(1, 7)],
