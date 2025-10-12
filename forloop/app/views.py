8e0409846d9e4e7cebeeadda056f563125173781
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-13 00:18:23 +0530

    final for lopp

--- forloop/app/views.py
+++ forloop/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def forl(request):
+    
+    data = {
+        'title': 'for loop',
+        'dis': ' welcom to for loop',
+        'fruits' :['mango','apple','graps', 'banana'],
+        'students' :[
+            {'name': 'amit', "mobile" : 123}
+        ]
+    }
+    return render(request,'index.html',data)
+
+
--- forloop/forloop/urls.py
+++ forloop/forloop/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from app import views as view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",view.forl)
 ]
