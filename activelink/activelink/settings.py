62a42183d2b8f71be4249c5ef26757b3fef975a9
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-22 19:23:27 +0530

    django

--- activelink/activelink/settings.py
+++ activelink/activelink/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = "activelink.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR , "templates" ],
+        
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -78,6 +79,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
@@ -120,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
