04ed36aebc605639e29f9acfe41073fabda4bb69
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-23 23:01:49 +0530

    GET Method django

--- getmtd/app/views.py
+++ getmtd/app/views.py
@@ -12,4 +12,20 @@ def services(request):
 def contact(request):
     return render(request, 'contact.html')
 def Userform(request):
+    try:
+        name = request.GET("name")
+        email = request.GET("email")
+        phone = request.GET("phone")
+        msg = request.GET("message")
+        print(name+ email+ phone+msg)
+    except:
+        pass
+    
+    # try:
+    #     n1 = request.GET['num1']
+    #     n2 = request.GET['num2']
+    #     print(n1+n2)
+    
+    # except:
+    #     pass
     return render(request, 'Userform.html')
